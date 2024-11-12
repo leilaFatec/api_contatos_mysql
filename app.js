46850169988f991b7d951945c66d15ac455c9697
leilaFatec <leila.cruz@fatec.sp.gov.br>
2024-11-11 21:09:14 -0300

    atualização port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const contatoRoutes = require('./routes/contatoRoutes');
 const initDB = require('./db/initDB');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
